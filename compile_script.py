25e26f84575362b7e6bf1d18ea08109d270b9ddc
dbarania <128425202+dbarania@users.noreply.github.com>
2025-05-22 09:07:07 +0200

    Added all files for the assignment

--- compile_script.py
+++ compile_script.py
@@ -1,11 +1,12 @@
 import os
 import subprocess
-import sys
 import time
 from typing import Literal
 from itertools import product
 
-BlockType = Literal["uint8_t", "uint16_t", "uint32_t", "uint64_t"]
+# It is assumed that this python script and main.c are in the same directory
+
+BlockType = Literal["uint8_t", "uint16_t", "uint32_t"]
 
 def compile(file, exec_name, block_type:BlockType="uint16_t"):
     gcc_command = [
@@ -32,7 +33,7 @@ def run(exec:str, length, number, seed):
     t2 = time.time()
     print(f"L = {number}, N = {length}, s = {seed}")
     print(result.stdout)
-    print(f"Program ran for {t2-t1}s\n")
+    print(f"Program ran for {t2 - t1}s\n")
 
 
 if __name__ =="__main__":
@@ -42,15 +43,13 @@ if __name__ =="__main__":
     c_file_path = f"{work_directory}/{c_file_name}"
     exec_name = "hamming_distance"
 
-    compile(c_file_path,exec_name)
+    compile(c_file_path, exec_name, "uint32_t")
 
     length_to_run = [64, 89, 256]
-    number_to_run = [100,1000]
+    number_to_run = [100, 1000]
     seed = 21
 
     for N,L in product(number_to_run, length_to_run):
-        run(exec_name,L,N,seed)
+        run(exec_name, L, N, seed)
 
-
-    #TODO Look into where the exec is saved how to change it
 
